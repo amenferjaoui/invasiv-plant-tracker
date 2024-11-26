08e7b2ac5f540b7908c99e7c9ceb6570e679836a
= <ihebboussida@gmail.com>
2024-11-26 11:14:36 +0100

    feat : connect to the plant id api

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,20 +5,31 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.json({ limit: '10mb' })); 
+app.use(express.json({ limit: '10mb' }));
 
 app.post('/process-image', (req, res) => {
-  const { image } = req.body;
+  const { image } = req.body.image;
+  const { latitude } = req.body.latitude;
+  const { longitude } = req.body.longitude;
 
   if (!image || typeof image !== 'string') {
     return res.status(400).send('Invalid or missing Base64 image in the request.');
   }
 
   try {
-    const buffer = Buffer.from(image, 'base64');
 
-    const outputPath = path.join(__dirname, 'output-image.png');
-    fs.writeFileSync(outputPath, buffer);
+    fetch("https://plant.id/api/v3/identification", {
+      headers: {
+        "Api-Key": process.env.apiKey,
+        "Content-Type": "application/json"
+      },
+      body: {
+        images: [image],
+        latitude: latitude,
+        longitude: longitude,
+        similar_images: true
+      }
+    })
 
     res.send({ message: 'Image received and processed successfully.' });
   } catch (error) {
